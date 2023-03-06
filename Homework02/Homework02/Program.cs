7eb55038752d299ecfe991690d83f1891f751f53
petkovM22 <petkovm22@gmail.com>
2023-03-06 15:46:19 +0100

    homework02

---
+++ Homework02/Homework02/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = new int[] { 4, 7, 10, 15, 12, 19 };
+int sum = 0;
+
+foreach (int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sum += number;
+    }
+}
+
+Console.WriteLine(sum);
---
+++ Homework02/StudentGroup/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] studentsG1 = new string[] { "Ana", "Martin", "Marko", "Stefan", "Nikola" };
+string[] studentsG2 = new string[] { "Ilija", "Toni", "Dragan", "Lazar", "Ivan" };
+
+Console.WriteLine("Enter student group: ");
+int groupNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if (groupNumber == 1)
+{
+    Console.WriteLine("Students in G1 are :");
+    foreach(string student in studentsG1)
+        Console.WriteLine(student);
+}
+else  if (groupNumber == 2)
+{
+    Console.WriteLine("Students in G2 are :");
+    foreach (string student in studentsG2)
+        Console.WriteLine(student);
+}
+else
+{
+        Console.WriteLine("Invalid input");
+}
